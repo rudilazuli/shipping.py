745852f347a1345fc907e24548634ea3e40f416c
rudilazuli <100395138+rudilazuli@users.noreply.github.com>
2022-04-18 07:44:02 -0700

    Create Sal's Shipping.py
    Comparing prices between ground shipping, ground premium, and drone shipping. Example is using weight at 41.5 lbs and 4.8 lbs packages; the goal is to compare the two and see which shipping option is cheaper.

---
+++ Sal's Shipping.py
@@ -0,0 +1,53 @@
+# Ground Shipping
+weight = 41.5
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight <=10:
+  cost_ground = weight * 4.00 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+print("Ground Shipping Price", cost_ground)
+
+cost_premium = 125.00
+print("Ground Shipping Premium $$$", cost_premium)
+
+# Drone Shipping
+weight = 41.5
+if weight <= 2:
+  cost_drone = weight * 4.50
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+print("Drone Shipping Price", cost_drone)
+
+ Ground Shipping
+weight = 4.8
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight <=10:
+  cost_ground = weight * 4.00 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+print("Ground Shipping Price", cost_ground)
+
+cost_premium = 125.00
+print("Ground Shipping Premium $$$", cost_premium)
+
+# Drone Shipping
+weight = 4.8
+if weight <= 2:
+  cost_drone = weight * 4.50
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+print("Drone Shipping Price", cost_drone)
